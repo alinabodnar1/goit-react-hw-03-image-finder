dde33eb6bb5d674190c0f0e247a37656a4fb3433
alinabodnar1 <malina.bodnar@gmail.com>
2023-05-12 09:07:06 +0300

    components

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Modal extends Component {
+
+    // componentDidMount() {
+	// 	document.addEventListener('keydown', this.handleESC)
+	// }
+
+	// componentWillUnmount() {
+	// 	document.removeEventListener('keydown', this.handleESC)
+	// }
+
+	// handleESC = (e) => {
+	// 	console.log('esc')
+	// 	if (e.code === 'Escape') {
+	// 		this.props.close()
+	// 	}
+    // }
+    
+    
+  render() {
+    return (
+      <div>Modal</div>
+    )
+  }
+}
